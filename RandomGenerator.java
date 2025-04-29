e68f86096d7c25777da85ecb9bed609cc86182a8
Sajid Kalam <sajidkalam@proton.me>
2025-04-29 12:27:14 +0530

    modified:   RandomGenerator.java

--- RandomGenerator.java
+++ RandomGenerator.java
@@ -1,21 +1,21 @@
-import java.util.Random;
+// import java.util.Random;
 
 
 
 
-class code {
-    public void generateNumbers() {
-        Random rand = new Random();
-        for (int i = 0; i < 5; i++) {
-            int randomNum = rand.nextInt(100) + 1; 
-            System.out.println(randomNum);
-        }
-    }
-}
+// class code {
+//     public void generateNumbers() {
+//         Random rand = new Random();
+//         for (int i = 0; i < 5; i++) {
+//             int randomNum = rand.nextInt(100) + 1; 
+//             System.out.println(randomNum);
+//         }
+//     }
+// }
 
-public class RandomGenerator{
-    public static void main(String[] args) {
-        code obj = new code();
-        obj.generateNumbers();
-    }
-}
+// public class RandomGenerator{
+//     public static void main(String[] args) {
+//         code obj = new code();
+//         obj.generateNumbers();
+//     }
+// }
